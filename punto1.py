c33fde8a9ce2f4c776625da714f2de49c91e1694
nparza <noelparzajuk@hotmail.com>
2018-10-26 19:17:26 -0300

    Item A:completo. Item B:sin silouhette

--- punto1.py
+++ punto1.py
@@ -38,7 +38,6 @@ Encuentro las particiones a partir de los diferentes métodos
 
 
 for label in labels:
-    t0= datetime.now()
     com = community(dolphins,label)
     comunidades[label]=com
 
@@ -55,19 +54,18 @@ Corré cada label a manopla, si no, se traba.
     
 colors = ['darkred','salmon','olivedrab','lightgreen','deepskyblue',
           'darkblue','blueviolet'] 
+#labels = ['louvain','fast_greedy','edge_betweenness','infomap']
+l = 'louvain'
 
-#labels = ['louvain']
-
-for l in labels:
-    for n,c in zip(dolphins.nodes,comunidades[l]):
-        dolphins.nodes[n]["comunidad"] = colors[int(c)]
-    plt.figure(labels.index(l))
-    nx.draw_networkx(dolphins,
-            node_color= list(nx.get_node_attributes(dolphins,'comunidad').values()), 
-            node_size=100, edge_color = 'grey', with_labels=False)
-    plt.title(l, loc='right',fontsize=12)
-    plt.axis('off')
-    plt.show(labels.index(l))
+for n,c in zip(dolphins.nodes,comunidades[l]):
+    dolphins.nodes[n]["comunidad"] = colors[int(c)]
+plt.figure(labels.index(l))
+nx.draw_networkx(dolphins,
+        node_color= list(nx.get_node_attributes(dolphins,'comunidad').values()), 
+        node_size=100, edge_color = 'grey', with_labels=False)
+plt.title(l, loc='right',fontsize=12)
+plt.axis('off')
+plt.show(labels.index(l))
 
 #%%
 
@@ -81,21 +79,24 @@ modularities = dict()
 #silhouettes = dict()  ##Todavía no hice esta parte del script
 randMod = dict()
 
+
 #%%
 
 '''
 Calculo la modularidad para cada particion obtenida.
-Comparo ese valor con las modularidades obtenidas a partir de 
-aplicar las particiones obtenidas en 1000 ('times') redes aleatorias 
+Comparo ese valor con las modularidades obtenidas de las particiones generadas
+en 1000 ('times') redes aleatorias 
 '''
 
 from networkx.algorithms.community import modularity
 
 times = 1000
 
-for l in labels:
+
+for l in labels:    
     for n,c in zip(dolphins.nodes,comunidades[l]):
         dolphins.nodes[n]["comunidad"] = int(c)
+        
     partitions = [set(grupo(dolphins,attribute='comunidad',kind=i)) for i in
                   range(1,int(max(comunidades[l]))+1)]
     modularities[l] = modularity(dolphins, partitions)
@@ -103,8 +104,13 @@ for l in labels:
     random_mode = []
     for t in range(times):
         newG = dolphins.copy()
-        newG = nx.double_edge_swap(dolphins, nswap=int(dolphins.number_of_nodes()/4), max_tries=500)
-        random_mode.append( modularity(newG, partitions))
+        newG = nx.double_edge_swap(dolphins, nswap=int(dolphins.number_of_nodes()/2), max_tries=500)
+        com = community(newG,label)
+        for n,c in zip(newG.nodes,com):
+            newG.nodes[n]["comunidad"] = int(c)
+        partitions = [set(grupo(newG,attribute='comunidad',kind=i)) for i in
+                      range(1,int(max(com))+1)]
+        random_mode.append(modularity(newG, partitions))
     
     randMod[l] = random_mode
     
@@ -118,13 +124,12 @@ Corré cada label a manopla, si no, se traba.
 '''
 #labels = ['louvain','fast_greedy','edge_betweenness','infomap']
     
-labels = ['infomap']
-    
-for l in labels:
-    plt.figure(labels.index(l))     
-    plt.scatter(range(times),randMod[l],marker='.', color='dodgerblue')
-    plt.axhline(modularities[l],marker='d',color='c',markersize=7,label=r'$Modularidad$')
-    plt.show(labels.index(l))
+l = 'louvain'
+
+plt.figure(labels.index(l))     
+plt.hist(randMod[l], color='dodgerblue', normed=True, histtype='step')
+plt.axvline(modularities[l],marker='d',color='c',markersize=7,label=r'$Modularidad$')
+plt.show(labels.index(l))
     
 
 #%%
